66f818cb14c27f9a8883d9d8a82c8cd180551d9b
Migs2312 <101750039+Migs2312@users.noreply.github.com>
2025-01-10 14:38:18 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ const developer = document.getElementById('devName');
 var moldura = document.getElementById('refm');
 var len = document.getElementById('length');
 var hei = document.getElementById('height');
+var ho = document.getElementById('hook');
 var glass = document.getElementsByName('glass');
 var verific = ""
 
@@ -26,7 +27,7 @@ function mold(){
 
 
     //Verificação de preenchimento
-        if(len.value<=0 || hei.value<=0 || isNaN(len.value)==true || isNaN(hei.value)==true || glassType==null || mValue==0){
+        if(len.value<=0 || hei.value<=0 || isNaN(len.value)==true || isNaN(hei.value)==true || glassType==null || mValue==0 || ho.value<=0 || isNaN(ho.value)==true){
             if(mValue==0){
                 verific = "Selecione uma moldura válida"
             }
@@ -36,10 +37,13 @@ function mold(){
             else if(hei.value<=0 || isNaN(hei.value)==true){
                 verific = "Preencha o campo Altura com um número válido"
             }
+            else if(ho.value<=0 || isNaN(ho.value)==true){
+                verific = "Preencha o campo Gancho com um número válido"
+            }
             else if(glassType==null){
                 verific = "Selecione um tipo de vidro"
             }
-            result.innerHTML = "<table id='money'><thead><th>Moldura</th><th>Medidas</th><th>Vidro</th><th>M. Obra</th><th>Total</th></thead><tbody><tr><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td><td></td></td><td></td></tr></tbody></table><br>"+verific
+            result.innerHTML = "<table id='money'><thead><th>Moldura</th><th>Medidas</th><th>Eucatex</th><th>Ganchos</th><th>Vidro</th><th>M. Obra</th><th>Total</th></thead><tbody><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td><td></td></td><td></td><td><td></td></td><td></td></tr></tbody></table><br>"+verific
         }
     //Verificação de preenchimento
 
@@ -54,8 +58,18 @@ function mold(){
                 heightFix = Number(hei.value.replace(",","."));
                 round(heightFix);
                 height = rounded;
-                perimeter = (((length*2)+(height*2)))/100;
+                perimeter = (((lengthFix.toFixed(2)*2)+(heightFix.toFixed(2)*2))+20)/100;
             //Tamanho da moldura
+
+            //Eucatex
+                eucaFix = ((height/100)*(length/100))*50;
+                round(eucaFix);
+                eucatex = rounded;
+            //Eucatex
+
+            //Gancho
+                hook = ho.value*5;
+            //Gancho
 
             //Valor do vidro
                 if(glassType=="True"){
@@ -76,7 +90,7 @@ function mold(){
             //Valor do vidro
 
             //Cálculo Final
-                totalPrim = ((mValue*(perimeter))+Number(glassValue));
+                totalPrim = ((mValue*(perimeter))+Number(glassValue)+Number(hook)+Number(eucatex));
                 mObra = totalPrim*(0.45)
 
                 totalFix = (Number(totalPrim)+Number(mObra));
@@ -86,7 +100,7 @@ function mold(){
         //Matemática
 
         //Conteúdo
-                result.innerHTML = "<table id='money'><thead><th>Moldura</th><th>Medidas</th><th>Vidro</th><th>M. Obra</th><th>Total</th></thead><tbody><tr><td>"+ref+"</td><td>"+length+"x"+height+"</td><td>"+glassName+"</td><td>45%</td></tr><tr><td>R$"+mValue+"</td><td>"+perimeter+"m</td><td>R$"+glassValue+"</td><td>R$"+mObra.toFixed(2)+"</td><td>R$"+total+"</td></tr></tbody></table>"
+                result.innerHTML = "<table id='money'><thead><th>Moldura</th><th>Medidas</th><th>Eucatex</th><th>Ganchos</th><th>Vidro</th><th>M. Obra</th><th>Total</th></thead><tbody><tr><td>"+ref+"</td><td>"+lengthFix+"x"+heightFix+"</td><td>"+length+"x"+height+"</td><td>"+ho.value+"un.</td><td>"+glassName+"</td><td>45%</td></tr><tr><td>R$"+mValue+"</td><td>"+perimeter+"m</td><td>R$"+eucatex+"</td><td>R$"+hook+"</td><td>R$"+glassValue+"</td><td>R$"+mObra.toFixed(2)+"</td><td>R$"+total+"</td></tr></tbody></table>"
         //Conteúdo
     }
 };
@@ -125,7 +139,7 @@ function moldValue(mold){
             mVal=0;
             mImg="none";
         break;
-        case "04BR_FL":
+        case "004BR":
             mVal=15;
             mImg="04BR_FL";
         break;
@@ -168,6 +182,42 @@ function moldValue(mold){
         case "031-700":
             mVal=35
             mImg="031700";
+        break;
+        case "031PFB":
+            mVal=27
+            mImg="031PFB";
+        break;
+        case "016PPF":
+            mVal=21
+            mImg="016PPF";
+        break;
+        case "073PF":
+            mVal=26
+            mImg="073PF";
+        break;
+        case "305PF_FL":
+            mVal=22
+            mImg="305PF_FL";
+        break;
+        case "198GRAY":
+            mVal=24
+            mImg="198GRAY";
+        break;
+        case "003MV_FL":
+            mVal=15
+            mImg="003MV_FL";
+        break;
+        case "014-403":
+            mVal=17
+            mImg="014403";
+        break;
+        case "221PF":
+            mVal=25
+            mImg="221PF";
+        break;
+        case "014PF_FL":
+            mVal=17
+            mImg="014PF_FL";
         break;
     };
     moldStor.push(mVal);
